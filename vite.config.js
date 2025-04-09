fcfea28338dd54126d012fee90d345856449a00b
DisLife373 <111551636+SasimaP@users.noreply.github.com>
2025-04-10 00:39:51 +0700

    Test deploy

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       "/api": {
-        target: "https://senuadpahdocker-production.up.railway.app",
+        target: "https://senuadpahdocker-production.up.railway.app/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
+        secure: false,
       },
     },
   },
