650474574183f03334ade470d3b4ae61b532928b
DisLife373 <111551636+SasimaP@users.noreply.github.com>
2025-04-10 00:26:27 +0700

    Fix bug

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://nuadpah-web.vercel.app",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
 });
