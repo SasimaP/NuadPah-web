2c1ec6dedeaae8441c80c1eb0c592611969d9a2b
DisLife373 <111551636+SasimaP@users.noreply.github.com>
2025-04-10 00:42:13 +0700

    Try constant baseUrl

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 const api = Axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://senuadpahdocker-production.up.railway.app/api",
   withCredentials: true,
 });
 
